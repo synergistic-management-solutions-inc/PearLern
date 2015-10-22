f0d052e16c00bf16e8ceb16f14b44c87ce3e1c07
Diandra Ryan-Mas <dryanmas@live.com>
2015-10-22 12:03:43 -0500

    expects POSTs to signup and signin

--- server/index.js
+++ server/index.js
@@ -4,46 +4,56 @@ var Path = require('path')
 
 var routes = express.Router()
 
-//
-// Provide a browserified file at a specified path
-//
+//redo once we have some public stuffs
 routes.get('/app-bundle.js',
   browserify('./client/app.js'))
 
-//
-// Example endpoint (also tested in test/server/index_test.js)
-//
+//redo once we have some public stuffs
 routes.get('/api/tags-example', function(req, res) {
   res.send(['node', 'express', 'browserify', 'mithril'])
 })
 
-//
-// Static assets (html, etc.)
-//
+routes.post('/signup', function(req, res){
+  var user = {};
+  user.username = req.body.username;
+  user.password = req.body.password;
+  
+  //actually create user
+  //grab ID from our friend the DB
+
+  //testing code, delete later
+  user.id = 1;
+
+  res.status(201).send(user);
+})
+
+routes.post('/signin', function(req, res){
+  var user = {};
+  user.username = req.body.username;
+  user.password = req.body.password;
+
+  //verify user exists/correct password
+
+  //testing code, delete later
+  user.id = 1;
+
+  res.status(200).send(user);
+})
+
 var assetFolder = Path.resolve(__dirname, '../client/public')
 routes.use(express.static(assetFolder))
 
-
 if (process.env.NODE_ENV !== 'test') {
-  //
-  // The Catch-all Route
-  // This is for supporting browser history pushstate.
-  // NOTE: Make sure this route is always LAST.
-  //
+
   routes.get('/*', function(req, res){
     res.sendFile( assetFolder + '/index.html' )
   })
 
-  //
-  // We're in development or production mode;
-  // create and run a real server.
-  //
+
+
   var app = express()
 
-  // Parse incoming request bodies as JSON
   app.use( require('body-parser').json() )
-
-  // Mount our main router
   app.use('/', routes)
 
   // Start the server!
@@ -52,6 +62,6 @@ if (process.env.NODE_ENV !== 'test') {
   console.log("Listening on port", port)
 }
 else {
-  // We're in test mode; make this file importable instead.
+
   module.exports = routes
 }
