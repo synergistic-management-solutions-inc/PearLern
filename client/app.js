4f16a16531f753b5b940076c95cdeb1e55ecf01a
Scott Schwartz <sschwa12@gmail.com>
2015-10-23 15:46:32 -0500

    consolidate app structure

--- client/app.js
+++ client/app.js
@@ -1,81 +1,13 @@
-var React = require('react')
+var React = require('react');
 var ReactDOM = require('react-dom');
 var Profile = require('./views/profile.js');
+var SignUp = require('./views/signup.js');
 var $ = require('jquery');
 
 
-var SignUp = React.createClass({
-  getInitialState: function(){
-    return {
-      username: 'Choose a username',
-      password: 'Choose a password'
-    }
-  },
-  addUser: function(user) {
-    //send user to router to -> server
-  },
-  render: function(){
-    return (
-        <div className="container">
-          <h2 className="signupHeader">Sign Up and Prepare to Get Schooled</h2>
-          <AddUsername addNew={this.addUser}/>
-          <AddPassword addNew={this.addUser}/> <br />
-          <button onClick={this.addUser}>Register</button>
-          <p className="loginRedirect">Already registered? <a href=''>Sign-In </a></p>
-        </div>
-      )
-  }
-})
-
-var AddUsername = React.createClass({
-  getInitialState: function(){
-    return {
-      newUser: ''
-    }
-  },
-  updateNewUser: function(event){
-    this.setState({
-      newUser: event.target.value
-    });
-  },
-  handleAddNew: function(){
-    this.props.addNew(this.state.newUser);
-    this.setState({
-      newUser: ''
-    });
-  },
-  render: function(){
-    return (
-      <div>
-        <input type="text" value={this.state.newUser} placeholder="Choose a Username" onChange={this.updateNewUser} />
-      </div>
-      )
-  }
-});
-
-var AddPassword = React.createClass({
-  getInitialState: function(){
-    return {
-      newPassword: ''
-    }
-  },
-  updateNewPassword: function(event){
-    this.setState({
-      newPassword: event.target.value
-    });
-  },
-  render: function(){
-    return (
-        <div>
-          <input type="text" value={this.state.newPassword} placeholder="Choose a Password" onChange={this.updateNewPassword} />
-        </div>
-      )
-  }
-});
-
-
-
-ReactDOM.render(<div><SignUp /><Profile users={Profile.USERS} /></div>, document.getElementById('app'));
+ReactDOM.render(<div><SignUp />
+                  <Profile users={Profile.USERS} />
+                </div>, document.getElementById('app'));
 
 // var App = React.createClass({
 //  render: function () {
