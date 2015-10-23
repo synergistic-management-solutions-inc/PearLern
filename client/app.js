238d3f366fbd37dcab11cada95257afed1241914
Diana <dianafaye17@gmail.com>
2015-10-23 17:14:34 -0500

    Added otherUsers view with the beginnings of the functionality

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Profile = require('./views/profile.jsx');
-var SignUp = require('./views/signup.jsx')
+var SignUp = require('./views/signup.jsx');
+var AllUsers = require('./views/otherUsers.jsx');
 var $ = require('jquery');
 
 ReactDOM.render(<div>
                   <SignUp />
                   <Profile users={Profile.USERS} />
+                  // <AllUsers />
                 </div>,
                   document.getElementById('app')
                 );
