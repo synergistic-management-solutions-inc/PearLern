ef1d40c67585cc205128052ee706398ac5632074
NPoling <npoling@gmail.com>
2015-10-23 22:18:21 -0500

    Refactored signup view.  Signup works & posts user object to server.

--- client/views/signup.jsx
+++ client/views/signup.jsx
@@ -1,123 +1,60 @@
 var React = require('react')
-var ReactDOM = require('react-dom');
+var ReactDOM = require('react-dom')
+var $ = require('jquery')
+var mountNode = document.getElementById('app');
 
-//TODOs:
-//Compose new user instance
-//Add routing to send out data
+//TODOs: 
+//Hide user password
 //Add form validation
-//consider adding novaldiate to forms to override default HTML 5 validation
+//Add novaldiate to forms to override default HTML 5 validation?
 
-var SignUpContainer = React.createClass({
+var SignUp = React.createClass({
   getInitialState: function (){
     return {
       username: '',
       password: ''
     }
   },
-  AddUser: function () {
-    //Composes new user instance from current username/password state
-    //var newUser = {};
-    //newUser.username = {this.state.username}
-    //newUser.password = {this.state.password}
-    //POST request goes here
-  },
-  AddUsername: function (username) {
-    //Property called by children to update state of parent
-    this.setState({
-      username: username
+  addUser: function () {
+    //Composes new user instance from current username/password state & posts to server
+    var newUser = {
+      username: this.state.username,
+      password: this.state.password
+    } 
+    $.ajax({
+      type: 'POST',
+      url: '/signup',
+      data: newUser
+    })
+    .then(function (err,res){
+      if (err) {
+        console.log(err)
+      } //send 20? on success?
     });
   },
-  AddPassword: function (password) {
-    //Property called by children to update state of parent
+  updateUsername: function (event) {
     this.setState({
-      password: password
+      username: event.target.value.substr(0, 30)
+    });
+  },
+  updatePassword: function (event) {
+    this.setState({
+      password: event.target.value.substr(0, 30)
     });
   },
   render: function () {
-    //do we want the addNew property on the form element?  Or elsewhere?
-    //not sure if enctype is correct or if we even want any of this default behavior in here
-    //Maps addNewUser/addNewUsername/addNewPassword properties to local methods
     return (
       <div className="signup-container">
-        <h2 className="signup-header">Sign Up and Prepare to Get Schooled</h2>
-        <form enctype="multipart/form-data" method="POST" action="/signup" addNewUser={this.AddUser} addNewPassword={this.AddPassword} addNewUsername={this.AddUsername}> 
-          <UsernameInput />
-          <PasswordInput />
-          <SubmitButton />
-        </form>
-        <SignUpFooter />
+        <h2 className="signup-header">Sign Up and Prepare to Get Schooled</h2> 
+        <input type="text" className="username-input" placeholder="Choose a Username" onChange={this.updateUsername}/>
+        <input type="text" className="password-input" placeholder="Choose a Password"  onChange={this.updatePassword}/> 
+        <button type="submit" className="submit-button" onClick={this.addUser}> Register </button>
+        <p className="signup-footer">Already registered? <a href='/signin'>Sign-In </a></p>
       </div>
     )
   }
 });
 
-//--------------------STATEFUL CHILD COMPONENTS---------------------
+ReactDOM.render(<SignUp />, mountNode);
 
-var UsernameInput = React.createClass({
-  getInitialState: function () {
-    return {
-      username: ''
-    }
-  },
-  updateUsername: function (event){
-    //60 character limit for username
-    this.setState({
-      username: event.target.value.substr(0, 60)
-    });
-  },
-  handleAddUsername: function () {
-    //Passes down AddNewUsername method from parent via prop; child calls this to update parent's state
-    this.props.AddNewUsername(this.state.username)
-  },
-  render: function () {
-    return (
-      <input type="text" className="username-input" placeholder="Choose a Username" onChange={this.updateUsername}/>
-    )
-  }
-});
-
-var PasswordInput = React.createClass({
-  getInitialState: function () {
-    return {
-      password: ''
-    }
-  },
-  updatePassword: function (event) {
-    //30 character limit for password
-    this.setState({
-      password: event.target.value.substr(0, 30)
-    })
-  },
-  handleAddPassword: function () {
-    //Passes down AddUser method from parent via prop; child calls this to update parent's state
-    this.props.AddNewPassword(this.state.password)
-  },
-  render: function () {
-    return (
-     <input type="text" className="password-input" placeholder="Choose a Password"  onChange={this.updatePassword}/> 
-    )
-  }
-});
-
-//--------------------STATELESS (DUMB) CHILD COMPONENTS---------------------
-var SubmitButton = React.createClass({
-  submitUser: function () {
-    this.props.AddNewUser();
-  },
-  render: function () {
-    return (
-      <button type="submit" className="submit-button" onClick={this.submitUser}> Register </button>
-    )
-  }
-});
-
-var SignUpFooter = React.createClass({
-  render: function () {
-    return (
-      <p className="signup-footer">Already registered? <a href='/signin'>Sign-In </a></p>
-    )
-  }
-});
-
-
-module.exports = SignUpContainer;
+module.exports = SignUp;
