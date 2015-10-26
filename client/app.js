7cb310c68d8c359a4613b08c0b93d2c57e5654ac
NPoling <npoling@gmail.com>
2015-10-26 12:50:19 -0500

    Framed out landing page.  Added material-ui & materialize to sign-up page.  Added password validation to sign-up, but not yet working.

--- client/app.js
+++ client/app.js
@@ -3,10 +3,12 @@ var ReactDOM = require('react-dom');
 var Profile = require('./views/profile.jsx');
 var SignUp = require('./views/signup.jsx');
 var SignIn = require('./views/signin.jsx');
+var Landing = require('./views/landing.jsx');
 var AllUsers = require('./views/otherUsers.jsx');
 var $ = require('jquery');
 
-ReactDOM.render(<div>
+ReactDOM.render(<div className="container">
+                  <Landing />
                   <SignUp />
                   <Profile users={Profile.USERS} />
                   <AllUsers />
