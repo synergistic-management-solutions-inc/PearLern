3eae9124a951681e5218ac97bdebcc595fd42057
Diandra Ryan-Mas <dryanmas@live.com>
2015-10-28 12:00:51 -0500

    messenger displays for users without message history

--- client/views/messenger.jsx
+++ client/views/messenger.jsx
@@ -2,18 +2,16 @@ var React = require('react')
 var $ = require('jquery');
 
 /*TODO
-  [] restructure server GET
-  [] attach to main file 
-  [] POST on submit
-  [] GET real data
+  [] test ajax
+  [] get current user in a more formalized way
+  [] figure out how to pass in user state on redirect  
 */
 
-  //sample data
-  var contacts = [
-    {"username": "Shady Pete"},
-    {"username": "Helen of Troy"}
-  ]
+  //this should be a variable stored as a prop in our highest 
+  //component and passed down
+  var currentUser = 'User'
 
+//sample data
   var conversations = [
     {'user': 'Shady Pete',
     'messages': [
@@ -41,24 +39,46 @@ var $ = require('jquery');
   //an individual contact component
   var Contact = React.createClass({
     selectUser: function(){
-      this.props.userSelected(this.props.contact.username);
+      this.props.userSelected(this.props.contact);
     },
     render: function(){
       return (
-        <p onClick={this.selectUser}>{this.props.contact.username}</p>
+        <p onClick={this.selectUser}>{this.props.contact}</p>
       );
     }
   });
 
   //the component for the list for all the contacts
-  var ContactContainer = React.createClass({
+  var Contacts = React.createClass({
+    getInitialState: function(){
+      //hard-coded version
+      return {contacts: ['Shady Pete', 'Helen of Troy', 'The Wizard of Sound']}
+
+      /* real version
+      return $.ajax({
+        type: 'GET',
+        url: '/users'
+      })
+      .then(function(res){
+        var state = {contacts: []}
+        res.body.users.each(function(user){
+          if (user !== currentUser){            
+            state.contacts.push()
+          }
+        })
+
+        return state;
+      })
+      */
+
+    },
     userSelected: function(username){
       this.props.displayConversation(username);
     },
     render: function(){
       var userSelected = this.userSelected;
-      var contacts = this.props.contacts.map(function(contact){
-        return <Contact userSelected={userSelected} key={contact.username} contact={contact} />
+      var contacts = this.state.contacts.map(function(contact){
+        return <Contact userSelected={userSelected} key={contact} contact={contact} />
       })
 
       return (
@@ -72,11 +92,11 @@ var $ = require('jquery');
     }
   })
 
-  //the component for an individual username in a conversation
+  //the username of the other user in the conversation
   var User = React.createClass({
     render: function(){
       return (
-        <div>{this.props.message.from}: </div>
+        <h5>{this.props.user} </h5>
       );
     }
   })
@@ -92,11 +112,27 @@ var $ = require('jquery');
 
   //the component for submitting a new message
   var NewMessage = React.createClass({
+    sendMessage: function(text){
+      var message = {
+        'to': this.props.user,
+        'from': currentUser,
+        'text': this.refs.message.getDOMNode().value
+        };
+
+      $.ajax({
+        type: 'POST',
+        url: '/messages',
+        data: message,
+        success: function(res){
+          //should re-render page; 
+        }
+      })
+    },
     render: function(){
       return (
         <div>
-          <input type='text'></input>
-          <button>Send</button>
+          <input ref='message' type='text'></input>
+          <button onClick={this.sendMessage}>Send</button>
         </div>
       )
     }
@@ -105,14 +141,26 @@ var $ = require('jquery');
   //the component for the entire conversation with one other user
   var Conversation = React.createClass({
     render: function(){
+      console.log('props', this.props.conversation);
+
       var conversation = this.props.conversation.messages.map(function(message){
-        return <p><User key={message.from} message={message} /><Message key={message.text} message={message} /></p>
+        //checks if the message in incoming or outgoing
+        //for styling purposes
+        if (message.to === currentUser){
+          var className = 'incoming';
+        }
+        else {
+          var className = 'outgoing';
+        } 
+
+        return <div className={className}><Message key={message.text} message={message} /></div>
       })
 
       return (
         <div>
+          <User user={this.props.conversation.username}/>
           {conversation}
-          <NewMessage /> 
+          <NewMessage user={this.props.conversation.username}/> 
         </div>
         ) 
     }
@@ -120,22 +168,35 @@ var $ = require('jquery');
 
   //the component for every conversation
   var Conversations = React.createClass({
+    getInitialState: function(){
+      //hard coded version:
+      return {conversations: conversations}
+
+      /*real version
+      return $.ajax({
+        type: 'GET',
+        url: '/messages/'+currentUser
+      })
+      .then(function(res){
+        return res.body; 
+      })
+      */
+      
+    },
     render: function(){
       var user = this.props.user;
+      var conversation = {username: user, messages: []}
 
-      //finds and displays the conversation that matches 
-      //currently selected user
-      var conversations = this.props.conversations
-        .filter(function(conversation){
-          return (conversation.user === user)
-        })
-        .map(function(conversation){
-          return  <Conversation key={conversation.messages} conversation={conversation} />
-        })
+      //grabs any messages from the state
+      this.state.conversations.forEach(function(conv){
+        if (conv.user === user){
+          conversation.messages = conv.messages;
+        }
+      })
 
       return (
         <div>
-          {conversations}
+          <Conversation key={conversation.messages} conversation={conversation} />
         </div>
       )
     }
@@ -145,7 +206,8 @@ var $ = require('jquery');
   var Messenger = React.createClass({
     getInitialState: function(){
       return {
-        user: conversations[0].user
+        //should be able to take an initial state
+        user: this.initialState || conversations[0].user
       }
     },
     displayConversation: function(username){
@@ -155,8 +217,8 @@ var $ = require('jquery');
       var displayConversation = this.displayConversation;
       return (
         <div>
-          <ContactContainer displayConversation={displayConversation} user={this.state.user} contacts={contacts} />
-          <Conversations user={this.state.user} conversations={conversations} />
+          <Contacts displayConversation={displayConversation} user={this.state.user} />
+          <Conversations user={this.state.user} />
         </div>
       )
     }
