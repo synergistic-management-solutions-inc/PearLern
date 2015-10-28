c29937306c58106deff68b8fb37913423474d7b3
Diana <dianafaye17@gmail.com>
2015-10-28 15:47:53 -0500

    Completely refactored otherUsers. Not in working state.

--- client/views/otherUsers.jsx
+++ client/views/otherUsers.jsx
@@ -1,91 +1,58 @@
 var React = require('react')
-var ReactDOM = require('react-dom');
 var $ = require('jquery');
 
-var datastuffs = {users: [
-  {
-    "username": "cottoncandy",
-      "email": "unicorn@unicorn.com",
-      "about": "I like things.",
-      "interests": ["python"]
-  },
-  {
-    "username": "puppies",
-      "email": "unicornpuppies@unicorn.com",
-      "about": "I like puppies.",
-      "interests": ["python"]
-  },
-  {
-    "username": "blargl",
-      "email": "unicornpuppiesdlfjsdklfj@unicorn.com",
-      "about": "I like puppies.",
-      "interests": ["ruby"]
-  }
-]}
 
-// TODO replace example email address
-var AllUsers = React.createClass({
+var otherUsers = React.createClass({
+  // Setting default state to empty strings to avoid errors
+  getInitialState: function() {
+    return { 
+      usernameValue: '',
+      interestsValue: '',
+      emailValue: ''
+    };
+  },
+
   componentDidMount: function () {
+    // The docs said to do ajax in componentDidMount
     var self = this;
-    console.log('hitting');
+    // Doing a GET request to /users
     $.ajax({
-      type : 'GET',
-      contentType : 'json',
-      url : '/users',
-      success: function(result) {
-        if (self.isMounted()){
+      type: 'GET',
+      url: '/users/',
+
+      // TO DO: Error handling
+      success: function(res) {
+        if (self.isMounted()) {
           self.setState({
-            username: result.users[0].username,
-            interests: result.users[0].interests
-          })
+            users: res.users[0],
+            usernameValue: res.users[0].username,
+            interestsValue: res.users[0].interests,
+            emailValue: res.users[0].email
+          });
         }
-        console.log("result:", result)
-      },
-      error: function(err){
-        console.log("Error:", err)
       }
-    })
+    });
   },
+
   render: function () {
-    // console.log(JSON.stringify(datastuffs.users));
-    return (
-      <div className="other-users-container">
-        <button onClick={this.getUsers}>fdsfjskfj</button>
-        <ul>
-          {datastuffs.users.map(function(element) {
-            return (<li>
-              <p>Username: {JSON.stringify(element.username).replace(/"|'/g,"")}</p>
-              <p>Interests: {JSON.stringify(element.interests).replace(/[\[\]"|']+/g, "")}</p>
-              <p>
-                Email: <a href="mailto:someone@example.com">{JSON.stringify(element.email).replace(/"|'/g,"")}</a>
-              </p>
-              <br />
-            </li>)
-          })}
-        </ul>
-      </div>
-
-    )
+    var users = this.state.interestsValue.map(function(user) {
+      return <User key={user} user={user} />
+    })
+    var username = this.state.usernameValue;
+    var interests = this.state.interestsValue;
+    var email = this.state.emailValue;
+    return (<div>
+              <div className="user-container">
+                <div className="username-label">Username:</div>
+                <div className="username">{username}</div>
+                <div className="interests-label">Interests</div>
+                <div className="interests">{interests}</div>
+                <div className="email-label">Email:</div>
+                <div className="email">{email}</div>
+              </div>
+            </div>
+            );
   }
-});
+})
 
-
-// TO DO: Add AJAX stuff, replace dummy data with real data, implement mapping
-var OneUser = React.createClass({
-    getInitialState: function() {
-      return {
-        username: '',
-        interest: ''
-      };
-    },
-    // Need component did mount for AJAX requests. Remember to check if component did mount.
-    render: function() {
-        return (
-            <div>Username: {this.state.username}, Interests: {this.state.interests}</div>
-        );
-    }
-});
-
-// ReactDOM.render(<AllUsers />, document.getElementById('app'));
-
-module.exports = AllUsers;
+module.exports = otherUsers;
