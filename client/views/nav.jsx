b216e9e4de1c8f76a8cb201bcdd763c751f60fac
rsoule3 <robsoule3@gmail.com>
2015-11-05 10:19:47 -0600

    signin/login showing on smaller screen

--- client/views/nav.jsx
+++ client/views/nav.jsx
@@ -31,7 +31,7 @@ var Nav = React.createClass({
     var links = function(){
       if (currentUser){
         return (
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <ul id="nav-mobile" className="right">
             <li><Link to="/profile">My Profile</Link></li>
             <li><Link to="/all">Find a Partner</Link></li>
             <li><Link to="/messenger">Messenger</Link></li>
@@ -40,7 +40,7 @@ var Nav = React.createClass({
         )
       } else {
         return (
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <ul id="nav-mobile" className="right">
             <li><Link to="/signin">Login</Link></li>
             <li><Link to="/signup">Register</Link></li>
           </ul>
@@ -51,11 +51,11 @@ var Nav = React.createClass({
       <nav>
           <div className="nav-wrapper light-blue">
             <img className="pear-logo img-responsive" src="http://www.clker.com/cliparts/y/p/p/E/u/H/green-pear.svg" />
-            <a href="#" className="brand-logo">PearLern</a>
+            <a href="#" className="pear-logo brand-logo hide-on-med-and-down">
+              PearLern</a>
             {links()}
           </div>
         </nav>
-
     )
   }
 });
