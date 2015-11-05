ce82c9da5b143e278c16fc9db4f2d7fd8facb080
James Hicks <jehicks2@yahoo.com>
2015-11-05 09:38:58 -0600

    Working on Profile

--- client/views/otherUsers.jsx
+++ client/views/otherUsers.jsx
@@ -3,8 +3,6 @@ var React = require('react');
 var $ = require('jquery');
 const RaisedButton = require('material-ui/lib/raised-button');
 var Link = require('react-router').Link;
-var userActions = require('../actions/userActions');
-var userStore = require('../stores/userStore')
 
 
 // Top-level component
@@ -20,10 +18,10 @@ var UserView = React.createClass({
 
   componentWillMount: function(){
     //this is a pretty hacky fix to the fact
-    //that we don't know how sessions work
+    //that we don't know how sessions work 
     //in passport
-    if (!this.props.currentUser){
-      this.props.history.pushState(null, '/signin');
+    if (!this.props.currentUser){ 
+      this.props.history.pushState(null, '/signin'); 
     }
   },
 
@@ -90,8 +88,8 @@ var Users = React.createClass({
 
       success: function(res) {
         if (self.isMounted()) {
-          self.setState({users: res.users,
-            message: self.props.message,
+          self.setState({users: res.users, 
+            message: self.props.message, 
             currentUser: self.props.currentUser})
         }
       }
@@ -107,7 +105,7 @@ var Users = React.createClass({
       <div>
         {this.state.users
           .filter(function (element) {
-            // filters out current user
+            // filters out current user 
             if (element.username == currentUser){
               return false;
             }
@@ -120,7 +118,7 @@ var Users = React.createClass({
               return true;
             }
 
-            // filters to show only users whose
+            // filters to show only users whose 
             // interests contain the search query
             return element.interests
             .some(function(interest){
@@ -130,8 +128,8 @@ var Users = React.createClass({
             })
           })
           .map(function (element) {
-            return <User  key={element.username}
-                          user={element}
+            return <User  key={element.username} 
+                          user={element} 
                           message={message} />
           })
         }
@@ -164,7 +162,7 @@ var User = React.createClass({
           </li>
         </ul>
         <Link to="/messenger">
-          <RaisedButton onClick={this.openMessenger}
+          <RaisedButton onClick={this.openMessenger} 
                         label="Message" />
         </Link>
       </div>
