4df83db1092e0bfcdb80fc3300c367530f58f8e8
James Hicks <jehicks2@yahoo.com>
2015-11-05 19:48:43 -0600

    Landing Page basic style

--- client/views/landing.jsx
+++ client/views/landing.jsx
@@ -13,11 +13,11 @@ var Landing = React.createClass({
       <div className="container">
            <img className="responsive-img" src="/images/LandingImage1.jpg"/>
         <div className="row">
-            <div className="col s12 s6">
+            <div className="col s12">
               <h3 className = "landing-header">Find People to Learn With!</h3>
             </div>
         </div>
-          <div className="text-header col s6 offset-s3">
+          <div className="text-header col s1">
             <p>PearLern helps match you with other people sharing the same learning objectives.  For a hefty fee, we will find you an accountabili-buddy to join you on your journey to learn the art of coding!</p>
           </div>
         <div className="push"></div>
